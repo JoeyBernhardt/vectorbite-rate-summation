9669c9676ed3f9572f056c1aec1ef5e294a24f02
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2019-03-07 20:34:14 -0800

    some initial plots

--- R-scripts/02_initial_plotting_joey.R
+++ R-scripts/02_initial_plotting_joey.R
@@ -12,10 +12,14 @@ tdata <- read_csv("Data/ExtractedDataAllStudies.csv")
 
 tdata2 <- tdata %>% 
 	mutate(study_digits = nchar(study_ID)) %>% 
+	mutate(mean_temp_calculated = (min_temp + max_temp)/2) %>% 
+	mutate(mean_temp_calculated = ifelse(temp_regime == 0, mean_temp, mean_temp_calculated)) %>% 
+	mutate(mean_temp_calculated = ifelse(is.na(mean_temp_calculated), mean_temp, mean_temp_calculated)) %>% 
 	filter(study_digits == 3)
 
 
 tdata2 %>% 
-	filter(study_ID == 102) %>% 
-	ggplot(aes(x = mean_temp, y = response, color = factor(temp_regime))) + geom_point() +
-	facet_wrap( ~ study_ID, scales = "free")
+	filter(temp_regime == 0) %>% 
+	ggplot(aes(x = mean_temp_calculated, y = response, color = trait)) + geom_point() +
+	facet_wrap(trait ~ study_ID, scales = "free")
+ggsave("figures/constant-temp-plots.png", width = 16, height = 8)
